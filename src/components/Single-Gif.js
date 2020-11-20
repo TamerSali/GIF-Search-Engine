60d08685dd9623da8299a4f3008a61da8ea00f64
Tamer Sali <tamerrsali@gmail.com>
2020-11-20 22:49:21 +0200

    Component (display gif content)

--- src/components/Single-Gif.js
+++ src/components/Single-Gif.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import PropTypes from 'prop-types'
+import { GifContext } from '../context';
 
-export default function SingleGif() {
-    return (
-        <div>
+export default function SingleGif({ url, title }) {
+	const [isImageReady, setIsImageReady] = useState(false);
+	const contextData = useContext(GifContext)
+	const { searchTerm } = contextData;
 
-        </div>
-    )
+	return (
+		<article className="single-gif">
+			<a href={url} target="_blank">
+				<img
+					src={url}
+					alt={title}
+					className={!isImageReady ? "ready-image" : ''}
+					onLoad={() => setIsImageReady(true)}
+				/>
+			</a>
+			<p style={isImageReady ? { display: "block" } : { display: "none" }} > { title || searchTerm}</p>
+		</article>
+	)
+}
+
+SingleGif.propTypes = {
+	/**
+	 * URL of Gif.
+	 *
+	 * @var {String}
+	 */
+	url: PropTypes.string,
+
+	/**
+	 * Title of Gif.
+	 *
+	 * @var {String}
+	 */
+	title: PropTypes.string
 }
