496bf17763ef2bc742a29cf980ae3b6d8ccdecf8
Tamer Sali <tamerrsali@gmail.com>
2020-11-20 22:50:14 +0200

    Component (search gif)

--- src/components/Search-Gif.js
+++ src/components/Search-Gif.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useRef, useContext } from 'react'
+import { GifContext } from '../context';
 
 export default function SearchGif() {
-    return (
-        <div>
-            
-        </div>
-    )
+	const InputRef = useRef();
+	const contextData = useContext(GifContext);
+	const { setSearchTerm } = contextData;
+
+	/**
+	 *  Handle user input.
+	 *
+	 * @return {Void}
+	 */
+	const handleChange = e => {
+		if (!InputRef.current.value) {
+			setSearchTerm("hello");
+		}
+		else {
+			setSearchTerm(InputRef.current.value)
+		}
+	}
+	return (
+		<header className="search-gif">
+			<div className="input-container">
+				<input
+					type="text"
+					placeholder="Search gif..."
+					ref={InputRef}
+					onChange={handleChange}
+				/>
+			</div>
+		</header>
+	)
 }
