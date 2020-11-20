4dcb34b52222e40d61f6310d4b288af4e99271bb
Tamer Sali <tamerrsali@gmail.com>
2020-11-20 14:21:43 +0200

    app component

---
+++ src/components/Gifs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Gifs() {
+    return (
+        <header class="search-gif">
+            <div className="input-container">
+
+            </div>
+            <input type="text" placeholder="Search gif..."/>
+        </header>
+    )
+}
---
+++ src/components/Search-Gif.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SearchGif() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
