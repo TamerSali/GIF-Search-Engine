ca7adab2181cc4b7189b049bd6ae12928fb7b26f
Tamer Sali <tamerrsali@gmail.com>
2020-11-22 16:48:40 +0200

    wrapped main component with context API

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+import ContextApi from './context'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextApi>
+      <App />
+    </ContextApi>
   </React.StrictMode>,
   document.getElementById('root')
 );
